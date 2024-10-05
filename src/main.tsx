1c8b3673e9d566a978d4edb647288df6be0d0f05
Andrii Livinskyi <landry2022@ukr.net>
2024-10-05 23:40:45 +0300

    установил @mui

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,59 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import * as React from "react";
+import * as ReactDOM from "react-dom/client";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+import "./styles/index.scss";
+// import "./index.css";
+
+import { VoiceoverProvider } from "./VoiceComponents/VoiceoverProvider.tsx";
+
+import ResponsiveAppBar from "./routes/ResponsiveAppBar/ResponsiveAppBar.tsx";
+import FamMealGuide from "./routes/ResponsiveAppBar/FamMealGuide.tsx";
+import ErrorPage from "./routes/Error/ErrorPage.tsx";
+
+// ************ Routers ************
+import routerFoodSupply from "./routerFoodSupply.tsx";
+import routerRecipes from "./routerRecipes.tsx";
+import routerRecommendations from "./routerRecommendations.tsx";
+// ************ Routers ************
+
+// {console.log(import.meta.env.BASE_URL);
+// }
+
+const router = createBrowserRouter(
+  [
+    //createBrowserRouter нужен для использования loader, action, errorElement
+    // и в паре с ними из компонента useLoaderData, useRouteError(),
+    // для формирования route может потребоваться и createRoutesFromElements
+    {
+      path: "/",
+      element: <ResponsiveAppBar />, // начинается отсюда
+      loader: () => "",
+      action: () => "",
+      errorElement: <ErrorPage />,
+      children: [
+        {
+          index: true,
+          element: <FamMealGuide />,
+        },
+        routerFoodSupply,
+        routerRecipes,
+        routerRecommendations,
+      ],
+    },
+  ],
+  {
+    basename: import.meta.env.BASE_URL,
+  }
+);
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <VoiceoverProvider>
+      {/* <App /> */}
+
+      <RouterProvider router={router} />
+    </VoiceoverProvider>
+  </React.StrictMode>
+);
--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -8,3 +8,8 @@
 // base: "/skipper-v3",
 
 // "noImplicitAny": false
+
+// npm i react-router-dom
+// npm install @mui/material @emotion/react @emotion/styled
+// npm install @mui/icons-material
+// npm install @fontsource/roboto
