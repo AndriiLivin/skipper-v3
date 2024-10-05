933ec4160758ec5b1301eac1431f313979f33c4f
Andrii Livinskyi <landry2022@ukr.net>
2024-10-05 22:50:31 +0300

    установил gh-peges

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,1 +1,8 @@
 /// <reference types="vite/client" />
+
+// npm install gh-pages --save-dev
+// сам установил его в devDependencies
+
+// "homepage": "https://AndriiLivin.github.io/skipper-v3",
+// "deploy": "yarn build && gh-pages -d dist"
+// base: "/skipper-v3",
